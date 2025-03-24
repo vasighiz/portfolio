5dd1f237d42131b783c96358eecdcc7f33d69453
Akram Vasighi <vasighi@uwindsor.ca>
2025-03-24 15:18:32 -0400

    Updated page theme

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -44,7 +44,7 @@ const Projects = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="space-y-6">
         {projects.map((project, index) => (
           <motion.div
             key={project.title}
